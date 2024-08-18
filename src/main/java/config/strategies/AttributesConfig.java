cfcaee6706d48ce011ab129fa1322f2bc9d36b12
Pr1nkos <prinkos1@gmail.com>
2024-08-19 00:26:42 +0300

    add: strategies for sections

---
+++ src/main/java/config/strategies/AttributesConfig.java
@@ -0,0 +1,20 @@
+package config.strategies;
+
+import java.util.Map;
+
+public class AttributesConfig implements ConfigStrategy {
+    private final Map<String, Object> attributes;
+
+    public AttributesConfig(Object config) {
+        if (config instanceof Map) {
+            this.attributes = (Map<String, Object>) config;
+        } else {
+            throw new IllegalArgumentException("Expected a Map for attributes configuration.");
+        }
+    }
+
+    @Override
+    public String getValue(String key) {
+        return attributes != null ? (String) attributes.get(key) : null;
+    }
+}
