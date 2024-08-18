a4a42c444123d4db04676778e3c641d895c4645a
Pr1nkos <prinkos1@gmail.com>
2024-08-19 00:34:43 +0300

    fix: reformat code

--- src/main/java/services/UserLoginService.java
+++ src/main/java/services/UserLoginService.java
@@ -13,30 +13,15 @@ public class UserLoginService implements LoginService {
     private final BCryptPasswordEncoder passwordEncoder;
 
     private List<UserDto> mockedFindAllUsers() {
-        return List.of(
-                UserDto.builder()
-                        .id(UUID.randomUUID())
-                        .email("user1@example.com")
-                        .password(passwordEncoder.encode("password1"))
-                        .build(),
-                UserDto.builder()
-                        .id(UUID.randomUUID())
-                        .email("user2@example.com")
-                        .password(passwordEncoder.encode("password2"))
-                        .build()
-        );
+        return List.of(UserDto.builder().id(UUID.randomUUID()).email("user1@example.com").password(passwordEncoder.encode("password1")).build(), UserDto.builder().id(UUID.randomUUID()).email("user2@example.com").password(passwordEncoder.encode("password2")).build());
     }
 
     private List<UserDto> mockedFindUsersByEmail(String email) {
-        return mockedFindAllUsers().stream()
-                .filter(user -> user.getEmail().equals(email))
-                .toList();
+        return mockedFindAllUsers().stream().filter(user -> user.getEmail().equals(email)).toList();
     }
 
     @Override
     public Optional<UserDto> getExistedUser(String email, String password) {
-        return mockedFindUsersByEmail(email).stream()
-                .filter(user -> passwordEncoder.matches(password, user.getPassword()))
-                .findFirst();
+        return mockedFindUsersByEmail(email).stream().filter(user -> passwordEncoder.matches(password, user.getPassword())).findFirst();
     }
 }
