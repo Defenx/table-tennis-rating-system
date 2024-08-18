808b842bec9a8b89405f916bafa2cdb32aed02b3
Pr1nkos <prinkos1@gmail.com>
2024-08-19 00:29:38 +0300

    add: Listener - appconfig, credentinalExtractor, userAuth      rename attributes. Now depends on config

--- src/main/java/listeners/InitializationListener.java
+++ src/main/java/listeners/InitializationListener.java
@@ -1,21 +1,36 @@
 package listeners;
 
-import Services.LoginService;
+import config.AppConfig;
+import jakarta.servlet.annotation.WebListener;
+import services.*;
 import jakarta.servlet.ServletContext;
 import jakarta.servlet.ServletContextEvent;
 import jakarta.servlet.ServletContextListener;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+@WebListener
 public class InitializationListener implements ServletContextListener {
+
     @Override
     public void contextInitialized(ServletContextEvent sce) {
         ServletContext servletContext = sce.getServletContext();
 
+        try {
+            AppConfig.loadConfig();
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to initialize application", e);
+        }
+
         BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
 
-        LoginService loginService = new LoginService(bCryptPasswordEncoder);
+        LoginService loginService = new UserLoginService(bCryptPasswordEncoder);
+        CredentialsExtractor credentialsExtractor = new BasicCredentialsExtractor();
 
-        servletContext.setAttribute("loginService", loginService);
+        UserAuthenticationService userAuthenticationService = new UserAuthManager(loginService);
+
+        servletContext.setAttribute(AppConfig.getCredentialsExtractorAttribute(), credentialsExtractor);
+        servletContext.setAttribute(AppConfig.getUserAuthServiceAttribute(), userAuthenticationService);
     }
+
 
 }
