3a94727f0ac033884f63f3acc1deccc5d70ef91e
Pr1nkos <prinkos1@gmail.com>
2024-08-19 00:30:13 +0300

    new: abstraction for loginService

---
+++ src/main/java/services/UserLoginService.java
@@ -0,0 +1,42 @@
+package services;
+
+import dto.UserDto;
+import lombok.AllArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@AllArgsConstructor
+public class UserLoginService implements LoginService {
+    private final BCryptPasswordEncoder passwordEncoder;
+
+    private List<UserDto> mockedFindAllUsers() {
+        return List.of(
+                UserDto.builder()
+                        .id(UUID.randomUUID())
+                        .email("user1@example.com")
+                        .password(passwordEncoder.encode("password1"))
+                        .build(),
+                UserDto.builder()
+                        .id(UUID.randomUUID())
+                        .email("user2@example.com")
+                        .password(passwordEncoder.encode("password2"))
+                        .build()
+        );
+    }
+
+    private List<UserDto> mockedFindUsersByEmail(String email) {
+        return mockedFindAllUsers().stream()
+                .filter(user -> user.getEmail().equals(email))
+                .toList();
+    }
+
+    @Override
+    public Optional<UserDto> getExistedUser(String email, String password) {
+        return mockedFindUsersByEmail(email).stream()
+                .filter(user -> passwordEncoder.matches(password, user.getPassword()))
+                .findFirst();
+    }
+}
