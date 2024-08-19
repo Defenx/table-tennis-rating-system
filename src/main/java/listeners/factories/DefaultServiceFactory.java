6acb4dd37cbf60a1847806668507694c58c6e309
Pr1nkos <prinkos1@gmail.com>
2024-08-19 16:48:57 +0300

    edit: service locator -> abstractFactory

---
+++ src/main/java/listeners/factories/DefaultServiceFactory.java
@@ -0,0 +1,28 @@
+﻿package listeners.factories;
+
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import services.*;
+
+public class DefaultServiceFactory implements ServiceFactory {
+
+    @Override
+    public BCryptPasswordEncoder createPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    public LoginService createLoginService(BCryptPasswordEncoder passwordEncoder) {
+        return new UserLoginService(passwordEncoder);
+    }
+
+    @Override
+    public UserAuthenticationService createUserAuthenticationService(LoginService loginService) {
+        return new UserAuthManager(loginService);
+    }
+
+    @Override
+    public CredentialsExtractor createCredentialsExtractor() {
+        return new BasicCredentialsExtractor();
+    }
+}
---
+++ src/main/java/listeners/factories/ServiceFactory.java
@@ -0,0 +1,11 @@
+﻿package listeners.factories;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import services.*;
+
+public interface ServiceFactory {
+    BCryptPasswordEncoder createPasswordEncoder();
+    LoginService createLoginService(BCryptPasswordEncoder passwordEncoder);
+    UserAuthenticationService createUserAuthenticationService(LoginService loginService);
+    CredentialsExtractor createCredentialsExtractor();
+}
