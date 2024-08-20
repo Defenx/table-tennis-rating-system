1758e8d49431735891913cb01d13d8831f3819c4
Pr1nkos <prinkos1@gmail.com>
2024-08-20 17:27:07 +0300

    fix: removed username from dto

--- src/main/java/dto/UserDto.java
+++ src/main/java/dto/UserDto.java
@@ -13,7 +13,6 @@ import java.util.UUID;
 @Builder
 public class UserDto {
     private UUID id;
-    private String username;
     private String password;
     private String email;
     private String firstName;
