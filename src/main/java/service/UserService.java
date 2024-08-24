703fbb99e20e04ec8717d8cb9cdc710680d16cb6
Pr1nkos <prinkos1@gmail.com>
2024-08-24 13:47:09 +0300

    fix:  @AllArgsConstructor -> @RequiredArgsConstructor fix: logic getExistedUser to stream add: empty line in the end

--- src/main/java/service/UserService.java
+++ src/main/java/service/UserService.java
@@ -5,26 +5,21 @@ import dto.UserDto;
 import entity.User;
 import enums.Role;
 import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class UserService {
     private final UserDao userDao;
     private final BCryptPasswordEncoder passwordEncoder;
 
 
     public Optional<User> getExistedUser(String email, String password) {
-        Optional<User> userOptional = userDao.findByEmail(email);
-        if (userOptional.isPresent()) {
-            User user = userOptional.get();
-            if (passwordEncoder.matches(password, user.getPassword())) {
-                return Optional.of(user);
-            }
-        }
-        return Optional.empty();
+        return userDao.findByEmail(email)
+                .filter(user -> passwordEncoder.matches(password, user.getPassword()));
     }
 }
