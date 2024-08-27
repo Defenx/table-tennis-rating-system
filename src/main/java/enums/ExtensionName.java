9c3271b61c3900a475b86f6ff3e499b0e0ac5e36
AVVoy <Aleksandr.V.Voynov@yandex.ru>
2024-08-27 22:19:42 +0300

    Added NUMBER_OF_SET_IN_WORKOUT, NUMBER_OF_SET_IN_PLAY_OFF, NUMBER_OF_PLAYERS, getDescription() and constructor

--- src/main/java/enums/ExtensionName.java
+++ src/main/java/enums/ExtensionName.java
@@ -1,5 +1,19 @@
 package enums;
 
 public enum ExtensionName {
-    IS_RATING
+    //TODO:
+//    добавить описание в конструкторе, посмотреть код
+    IS_RATING("1"),
+    NUMBER_OF_SET_IN_WORKOUT("2"),
+    NUMBER_OF_SET_IN_PLAY_OFF("3"),
+    NUMBER_OF_PLAYERS("4");
+    private final String description;
+    public String getDescription(){
+        return description;
+    }
+
+    ExtensionName(String description) {
+        this.description = description;
+    }
+
 }
