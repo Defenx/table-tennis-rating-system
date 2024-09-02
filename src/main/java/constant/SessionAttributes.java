3c05a4996ac61974466f67830c78f6f165432982
Lenovo <dima.kozak2008@gmail.com>
2024-09-02 12:18:34 -0700

    LoadSalt added

--- src/main/java/constant/SessionAttributes.java
+++ src/main/java/constant/SessionAttributes.java
@@ -3,7 +3,8 @@ package constant;
 public final class SessionAttributes {
 
     public static final String USER_SESSION_ATTRIBUTE = "user";
-
+    public static final String CSRF_SALT_CACHE_ATTRIBUTE = "csrfSaltCache";
+    public static final String SALT_ATTRIBUTE = "salt";
     private SessionAttributes() {
     }
 }
--- src/main/java/filter/csrf/LoadSalt.java
+++ src/main/java/filter/csrf/LoadSalt.java
@@ -1,4 +1,32 @@
 package filter.csrf;
 
-public class LoadSalt implements Filter{
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
+import constant.SessionAttributes;
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+import org.apache.commons.lang3.RandomStringUtils;
+
+import java.io.IOException;
+import java.security.SecureRandom;
+import java.util.concurrent.TimeUnit;
+public class LoadSalt implements Filter {
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest req = (HttpServletRequest) servletRequest;
+        HttpSession session = req.getSession();
+        Cache<String,Boolean> csrfSaltCache =(Cache<String,Boolean>) session.getServletContext().getAttribute(SessionAttributes.CSRF_SALT_CACHE_ATTRIBUTE);
+        if(csrfSaltCache==null){
+            csrfSaltCache = CacheBuilder.newBuilder()
+                    .maximumSize(5000)
+                    .expireAfterWrite(30, TimeUnit.MINUTES).build();
+            session.setAttribute(SessionAttributes.CSRF_SALT_CACHE_ATTRIBUTE, csrfSaltCache);
+        }
+        String salt = RandomStringUtils.random(20,0,0,true,true,null,new SecureRandom());
+        csrfSaltCache.put(salt,true);
+        req.setAttribute(SessionAttributes.SALT_ATTRIBUTE,salt);
+        filterChain.doFilter(req,servletResponse);
+    }
 }
