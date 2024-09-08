cda8609e335447fbfe5934999f289c45d4d59cb3
Lenovo <dima.kozak2008@gmail.com>
2024-09-07 17:01:58 -0700

    fix: refactor

--- src/main/java/filter/csrf/ValidateCsrfToken.java
+++ src/main/java/filter/csrf/ValidateCsrfToken.java
@@ -10,7 +10,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @SuppressWarnings(value = "unchecked")
-public class ValidateSalt extends BaseFilter {
+public class ValidateCsrfToken extends BaseFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException {
@@ -19,16 +19,11 @@ public class ValidateSalt extends BaseFilter {
         }
         String csrfToken = request.getParameter(SessionAttributes.CSRF_TOKEN_ATTRIBUTE);
 
-        System.out.println(csrfToken);
-
         Cache<String, Boolean> csrfPreventionSaltCache = (Cache<String, Boolean>) request
                 .getSession()
                 .getAttribute(SessionAttributes.CSRF_TOKEN_CACHE_ATTRIBUTE);
-
-        System.out.println(csrfPreventionSaltCache.asMap());
-
         if (
-                csrfPreventionSaltCache != null
+                        csrfPreventionSaltCache != null
                         && csrfToken != null
                         && csrfPreventionSaltCache.getIfPresent(csrfToken) != null
         ) {
