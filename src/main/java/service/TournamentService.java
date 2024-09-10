975c78704204aa06ddb49f200e75ec87c5cfc377
Toxaencom1 <Toxaencom@gmail.com>
2024-09-10 13:59:55 +0500

    Refactor ParticipantsSplitter methods (matchmaker and distributeParticipantsForMatches)

--- src/main/java/service/TournamentService.java
+++ src/main/java/service/TournamentService.java
@@ -90,41 +90,32 @@ public class TournamentService {
 
     public List<Match> matchmaker(Tournament tournament) {
         List<TournamentParticipant> participants = tournament.getParticipants();
-        if (!participants.isEmpty() && participants.size() % 2 == 0) {
+        int size = participants.size();
+        if (size != 0 && size % 2 == 0) {
             List<Match> listOfMatches = new ArrayList<>();
             participants.sort(Comparator.comparing((TournamentParticipant tp) -> tp.getUser().getRating()).reversed());
 
-            List<TournamentParticipant> firstPartParticipants;
-            List<TournamentParticipant> secondPartParticipants;
+            boolean even = (size / 2) % 2 == 0;
+            List<TournamentParticipant> firstPart = participants.subList(0, even ? (size / 2) : (size / 2) + 1);
+            List<TournamentParticipant> secondPart = participants.subList(even ? (size / 2) : (size / 2) + 1, size);
 
-            if ((participants.size() / 2) % 2 != 0) {
-                firstPartParticipants = participants.subList(0, participants.size() / 2 + 1);
-                secondPartParticipants = participants.subList(participants.size() / 2 + 1, participants.size());
-            } else {
-                firstPartParticipants = participants.subList(0, participants.size() / 2);
-                secondPartParticipants = participants.subList(participants.size() / 2, participants.size());
-            }
-
-            Collections.shuffle(firstPartParticipants);
-            Collections.shuffle(secondPartParticipants);
-            listOfMatches.addAll(distributeParticipantsForMatches(firstPartParticipants, tournament));
-            listOfMatches.addAll(distributeParticipantsForMatches(secondPartParticipants, tournament));
+            listOfMatches.addAll(distributeParticipantsForMatches(firstPart, tournament));
+            listOfMatches.addAll(distributeParticipantsForMatches(secondPart, tournament));
 
             return listOfMatches;
-
         } else
             throw new RuntimeException("The number of participants is not even");
     }
 
     public List<Match> distributeParticipantsForMatches(List<TournamentParticipant> participants, Tournament tournament) {
+        Collections.shuffle(participants);
         List<Match> listOfMatches = new ArrayList<>();
         for (int i = 0; i < participants.size(); i = i + 2) {
-            Match match = Match.builder()
+            listOfMatches.add(Match.builder()
                     .tournament(tournament)
                     .user1(participants.get(i).getUser())
                     .user2(participants.get(i + 1).getUser())
-                    .build();
-            listOfMatches.add(match);
+                    .build());
         }
         return listOfMatches;
     }
