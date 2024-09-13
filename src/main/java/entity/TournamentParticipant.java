6560e73a75584118c8cbe9ad4bdc7f7a04fc715f
k0ct0pka <150600777+k0ct0pka@users.noreply.github.com>
2024-09-13 01:46:18 -0700

    add: add_column_created_date_into_tournament_participant.sql , tag in… (#47)

--- src/main/java/entity/TournamentParticipant.java
+++ src/main/java/entity/TournamentParticipant.java
@@ -1,18 +1,16 @@
 package entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
+import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UuidGenerator;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Entity
@@ -38,4 +36,7 @@ public class TournamentParticipant {
     @ToString.Exclude
     private Tournament tournament;
 
+    @CreationTimestamp
+    @Column(name = "created_date")
+    private LocalDateTime createdDate;
 }
