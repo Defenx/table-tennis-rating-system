fd79e5c839c6c870e06c08765eec826b7e26bf88
Pr1nkos <prinkos1@gmail.com>
2024-09-22 18:27:00 +0300

    - Fix: moved MatchDao logic to TournamentDao

--- src/main/java/dao/TournamentDao.java
+++ src/main/java/dao/TournamentDao.java
@@ -1,8 +1,10 @@
 package dao;
 
 import entity.Extension;
+import entity.Match;
 import entity.Tournament;
 import lombok.RequiredArgsConstructor;
+import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
@@ -22,13 +24,19 @@ public class TournamentDao {
     }
 
     public void create(Tournament tournament) {
+        Transaction transaction = null;
         try (Session session = sessionFactory.openSession()) {
-            Transaction transaction = session.beginTransaction();
+            transaction = session.beginTransaction();
             for (Extension extension : tournament.getExtensions()) {
                 extension.setTournament(tournament);
             }
             session.persist(tournament);
             transaction.commit();
+        } catch (HibernateException e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw e;
         }
     }
 
@@ -36,14 +44,28 @@ public class TournamentDao {
         try (Session session = sessionFactory.openSession()) {
             Query<Tournament> query = session.createQuery("FROM Tournament t WHERE t.status = 'NEW'", Tournament.class);
             return query.getResultList();
+        } catch (HibernateException he) {
+            throw new RuntimeException(he);
         }
     }
 
     public void deleteTournament(Tournament tournament) {
+        Transaction transaction = null;
         try (Session session = sessionFactory.openSession()) {
-            Transaction transaction = session.beginTransaction();
+            transaction = session.beginTransaction();
             session.remove(tournament);
             transaction.commit();
+        } catch (HibernateException e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw e;
+        }
+    }
+
+    public Match getMatchById(UUID matchId) {
+        try (Session session = sessionFactory.openSession()) {
+            return session.get(Match.class, matchId);
         }
     }
 }
